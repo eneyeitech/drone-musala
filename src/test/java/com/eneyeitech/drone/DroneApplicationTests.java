09cc01a8ecc5ef59665c621bd1deb2f2f69cfd3f
eneyeitech <abdulmumineabdulkarim@gmail.com>
2022-08-17 17:51:44 +0100

    Spring Initialization

---
+++ src/test/java/com/eneyeitech/drone/DroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eneyeitech.drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
